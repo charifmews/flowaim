8594d69081600a46ad34a38f9fc3b6661fb784af
Charif Mews <charif@westaysafe.com>
2024-06-08 14:43:10 +0200

    Add simple login crossmark flow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
 <script>
-	import { source } from 'sveltekit-sse';
-	const value = source('/api').select('message');
+	// import { source } from 'sveltekit-sse';
+	// const value = source('/api').select('message');
+    import sdk from '@crossmarkio/sdk';
+    let value;
+    async function signIn() {
+        let { request, response, createdAt, resolvedAt } = await sdk.methods.signInAndWait();
+        console.log(response.data.address);
+        value = response.data.address;
+        console.log("value: ", value)
+    }
+ 
+// log address
 </script>
 
-{$value}
+<h1>Home</h1>
+{#if !value}
+<button on:click={signIn}>
+    Login
+</button>
+{/if}
+{value}
